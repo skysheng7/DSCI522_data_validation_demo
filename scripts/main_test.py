3e4deeccb8b2eea5aa33045853f1adc8c02e1d0c
skysheng7 <skysheng@mail.ubc.ca>
2025-12-02 13:48:43 -0800

    add demo

---
+++ scripts/main_test.py
@@ -0,0 +1,9 @@
+# my_script.py
+print(f"__name__ is: {__name__}")
+
+def my_function():
+    print("Hello from my_function!")
+
+if __name__ == "__main__":
+    print("Running as main script")
+    my_function()
